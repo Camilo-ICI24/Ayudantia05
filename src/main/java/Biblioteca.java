d06b4959716e608d19a19ad1f2cb8bb7d60a3fee
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-10-02 10:26:15 -0300

    Actualización: actualización de clase Biblioteca

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -9,6 +9,24 @@ public class Biblioteca {
         this.libros = new ArrayList<>();
     }
 
+    public Libro buscarLibroPorTitulo(String titulo) {
+        for (Libro libro : this.libros) {
+            if (libro.getTitulo().equals(titulo)) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    public Libro buscarLibroPorAutor(String autor) {
+        for (Libro libro : this.libros) {
+            if (libro.getAutor().equals(autor)) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
     public boolean agregarLibro (Libro libro) {
         if (libro.getISBN() == null) {
             this.libros.add(libro);
@@ -18,5 +36,6 @@ public class Biblioteca {
             return false;
         }
     }
+
 
 }
